a32fc4c287f8c694f86286684d6bd5300b7f12f1
Denis Diveev <ddiveev99@gmail.com>
2025-02-05 18:52:19 +0300

    Enhance make_session function with overloads for synchronous and asynchronous engines

--- clickhouse_sqlalchemy/orm/session.py
+++ clickhouse_sqlalchemy/orm/session.py
@@ -1,11 +1,22 @@
+from typing import Union, Type, overload, Literal
+
+from sqlalchemy import Engine
 from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
 
 from .query import Query
 
 
-def make_session(engine, is_async=False):
-    session_class = Session
+@overload
+def make_session(engine: Engine, is_async: Literal[False] = False) -> Session: ...
+
+
+@overload
+def make_session(engine: AsyncEngine, is_async: Literal[True]) -> AsyncSession: ...
+
+
+def make_session(engine: Union[Engine, AsyncEngine], is_async: bool = False) -> Union[Session, AsyncSession]:
+    session_class: Union[Type[Session], Type[AsyncSession]] = Session
     if is_async:
         session_class = AsyncSession
 
